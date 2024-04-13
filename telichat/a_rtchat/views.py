b5f17743df23206bb73124e4d5527e724562aa35
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-04-13 21:52:07 +0500

    craete chat components with htmx

--- telichat/a_rtchat/views.py
+++ telichat/a_rtchat/views.py
@@ -7,12 +7,20 @@ from .form import *
 def chat_view(request):
     chatGroup=get_object_or_404(ChatGroup,group_name='public-chat')
     chatMessages=chatGroup.chat_messages.all()[:30]
-    form=ChatMessageCreateForm(request.POST)
-    if form.is_valid():
-        message=form.save(commit=False)
-        message.author=request.user
-        message.group=chatGroup
-        message.save()
-        return redirect('home')
+    form=ChatMessageCreateForm()
+    # with htmx
+    if request.htmx:
+        form=ChatMessageCreateForm(request.POST)
+        if form.is_valid():
+            message=form.save(commit=False)
+            message.author=request.user
+            message.group=chatGroup
+            message.save()
+            # return redirect('home')
+            context = {
+                'message' : message,
+                'user' : request.user
+            }
+            return render(request, 'a_rtchat/partials/chat_message_p.html', context)
         
     return render(request , 'a_rtchat/chat.html' , {"chatMessages" : chatMessages,"form" : form})
