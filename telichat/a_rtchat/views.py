dde7005d8c0620539e721a8ac385d7eb1ebf4168
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-04-13 22:05:22 +0500

    clear basic chat app dynamically

--- telichat/a_rtchat/views.py
+++ telichat/a_rtchat/views.py
@@ -1,26 +1,26 @@
-from django.shortcuts import redirect, render,get_object_or_404
-from.models import *
+from django.shortcuts import render, get_object_or_404, redirect 
 from django.contrib.auth.decorators import login_required
-from .form import *
-# Create your views here.
+from .models import *
+from .form import ChatMessageCreateForm
+
 @login_required
 def chat_view(request):
-    chatGroup=get_object_or_404(ChatGroup,group_name='public-chat')
-    chatMessages=chatGroup.chat_messages.all()[:30]
-    form=ChatMessageCreateForm()
-    # with htmx
+    chatGroup = get_object_or_404(ChatGroup, group_name="public-chat")
+    chatMessages = chatGroup.chat_messages.all()[:30]
+    form = ChatMessageCreateForm()
+    
     if request.htmx:
-        form=ChatMessageCreateForm(request.POST)
-        if form.is_valid():
-            message=form.save(commit=False)
-            message.author=request.user
-            message.group=chatGroup
+        form = ChatMessageCreateForm(request.POST)
+        if form.is_valid:
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chatGroup
             message.save()
-            # return redirect('home')
+            #works with htmx
             context = {
                 'message' : message,
                 'user' : request.user
             }
             return render(request, 'a_rtchat/partials/chat_message_p.html', context)
-        
-    return render(request , 'a_rtchat/chat.html' , {"chatMessages" : chatMessages,"form" : form})
+    
+    return render(request, 'a_rtchat/chat.html', {'chat_messages' : chatMessages, 'form' : form})
