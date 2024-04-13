8f90074aab10e6ce8fe7514163d31f9432c01071
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2024-04-13 21:24:18 +0500

    send message module

--- telichat/a_rtchat/views.py
+++ telichat/a_rtchat/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render,get_object_or_404
+from.models import *
+from django.contrib.auth.decorators import login_required
+from .form import *
 # Create your views here.
+@login_required
 def chat_view(request):
-    return render(request , 'a_rtchat/chat.html')
+    chatGroup=get_object_or_404(ChatGroup,group_name='public-chat')
+    chatMessages=chatGroup.chat_messages.all()[:30]
+    form=ChatMessageCreateForm(request.POST)
+    if form.is_valid():
+        message=form.save(commit=False)
+        message.author=request.user
+        message.group=chatGroup
+        message.save()
+        return redirect('home')
+        
+    return render(request , 'a_rtchat/chat.html' , {"chatMessages" : chatMessages,"form" : form})
